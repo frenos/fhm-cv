7927ad9cd84d0b1dac46c20575524dd8c1c4f490
frenos <frenos@users.noreply.github.com>
2014-10-30 00:49:34 +0100

    fixed conversion methods

--- praktikum1/src/de/codepotion/cv/ImageHelper.java
+++ praktikum1/src/de/codepotion/cv/ImageHelper.java
@@ -2,6 +2,9 @@ package de.codepotion.cv;
 
 import java.awt.image.BufferedImage;
 import java.awt.image.DataBufferByte;
+import java.awt.image.DataBufferInt;
+import java.nio.ByteBuffer;
+import java.nio.IntBuffer;
 
 import javafx.embed.swing.SwingFXUtils;
 import javafx.scene.image.Image;
@@ -12,31 +15,39 @@ import org.opencv.core.Mat;
 public class ImageHelper {
 
 	/**
-	 *  Fast conversion, based on http://answers.opencv.org/question/10344/opencv-java-load-image-to-gui/?answer=26499#post-id-26499
-	 * @param input Mat to convert
+	 * Fast conversion, based on
+	 * http://answers.opencv.org/question/10344/opencv-
+	 * java-load-image-to-gui/?answer=26499#post-id-26499
+	 * 
+	 * @param input
+	 *            Mat to convert
 	 * @return converted Mat as javafx-image
 	 */
-	public static Image mat2Image(Mat input)
-	{
-		int type = BufferedImage.TYPE_BYTE_GRAY;
-        if ( input.channels() > 1 ) {
-            type = BufferedImage.TYPE_3BYTE_BGR;
-        }
-        int bufferSize = input.channels()*input.cols()*input.rows();
-        byte [] b = new byte[bufferSize];
-        input.get(0,0,b); // get all the pixels
-        BufferedImage output = new BufferedImage(input.cols(),input.rows(), type);
-        final byte[] targetPixels = ((DataBufferByte)output.getRaster().getDataBuffer()).getData();
-        System.arraycopy(b, 0, targetPixels, 0, b.length);  
-        return SwingFXUtils.toFXImage(output, null);
+	public static Image mat2Image(Mat inputMat) {
+		 int type = BufferedImage.TYPE_BYTE_GRAY;
+	        if ( inputMat.channels() > 1 ) {
+	            type = BufferedImage.TYPE_3BYTE_BGR;
+	        }
+	        
+	        byte [] data = new byte[inputMat.channels()*inputMat.cols()*inputMat.rows()];
+	        inputMat.get(0,0,data); // get all the pixels
+	        BufferedImage image = new BufferedImage(inputMat.cols(),inputMat.rows(), type);
+	        final byte[] targetPixels = ((DataBufferByte) image.getRaster().getDataBuffer()).getData();
+	        System.arraycopy(data, 0, targetPixels, 0, data.length);  
+	        
+		return SwingFXUtils.toFXImage(image, null);
 	}
-	
-	public static Mat image2Mat(Image input)
-	{
-		BufferedImage inputBuffered = SwingFXUtils.fromFXImage(input, null);
-		byte[] pixels = ((DataBufferByte)inputBuffered.getRaster().getDataBuffer()).getData();
-		Mat mat = new Mat(inputBuffered.getHeight(),inputBuffered.getWidth(), CvType.CV_8UC3);
-		mat.put(0, 0, pixels);
-		return mat;
+
+	public static Mat image2Mat(Image input) {
+		BufferedImage original = SwingFXUtils.fromFXImage(input, null);
+		
+		//always convert input to 3B BGR so we can copy direct to Mat
+		BufferedImage outputImage = new BufferedImage(original.getWidth(), original.getHeight(), BufferedImage.TYPE_3BYTE_BGR);
+		outputImage.getGraphics().drawImage(original, 0, 0, null);
+		
+		byte[] data = ((DataBufferByte) outputImage.getRaster().getDataBuffer()).getData();
+		Mat outputMat = new Mat(outputImage.getHeight(), outputImage.getWidth(), CvType.CV_8UC3);
+		outputMat.put(0, 0, data);
+	    return outputMat;
 	}
 }
